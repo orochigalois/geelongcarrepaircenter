ea43dc9ee94cc21334f839c573f68215dbc5c0ef
Alex Yin <fudanyinxin@gmail.com>
2019-01-17 22:55:40 +1100

    new db

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'geelongcarrepair');
+define('DB_NAME', 'geelongcarrepaircentre_database');
 
 /** MySQL database username */
 define('DB_USER', 'root');
